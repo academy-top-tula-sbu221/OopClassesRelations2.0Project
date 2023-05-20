9db550f10f61cb4264efcf7d658bce5f8d4fb6a3
maxefimov <efimov.top.academy@gmail.com>
2023-05-20 13:16:00 +0300

    Добавьте файлы проекта.

---
+++ OopClassesRelations2.0Project/OopClassesRelations2.0Project.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+class Flat 
+{
+public:
+    Flat() { std::cout << "Flat create\n"; }
+    ~Flat() { std::cout << "Flat destroy\n"; }
+};
+
+class Build
+{
+    Flat flat;
+public:
+    Build() { std::cout << "Build create\n"; }
+    ~Build() { std::cout << "Build destroy\n"; }
+};
+
+class Boss
+{
+public:
+    Boss() { std::cout << "Boss create\n"; }
+    ~Boss() { std::cout << "Boss destroy\n"; }
+};
+
+class Department
+{
+    Boss* boss;
+public:
+    Department(Boss* boss = nullptr) : boss{ boss } { std::cout << "Department create\n"; }
+    ~Department() { std::cout << "Department destroy\n"; }
+};
+
+int main()
+{
+    //Build build;
+    Boss* boss = new Boss;
+    Department dep(boss);
+}
